4b30ec1ac8cbed89267aec37c0528f94b3a87f3d
jd9913 <67403842+jd9913@users.noreply.github.com>
2021-01-14 12:51:06 -0700

    update card in inventory

--- resource-client/src/components/Item/index.js
+++ resource-client/src/components/Item/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Card } from 'react-bootstrap';
 
-function Item( { item } ) {
+function Item({ item }) {
     return (
         <Card className='my-3 p-3 rounded'>
             <a href={`/inventory/${item._id}`}></a>
-            
+            <Card.Body>
+                <a href={`/inventory/${item._id}`}>
+                    <Card.Title as='div'><strong>{item.name}</strong>
+
+                    </Card.Title>
+                </a>
+                <Card.Text as='div'>{item.totQuantity}</Card.Text>
+            </Card.Body>
+
         </Card>
     )
 }
